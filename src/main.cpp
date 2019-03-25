064f70e3e7152a989a79d6dfafde9aaaf7a91ba9
Basu Dubey <basu.96@gmail.com>
2019-03-25 16:36:07 +0530

    Add more primitive methods

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "graphix.h"
 #include "primitives.h"
 
-//#include "primitives.h"
+#include "primitives.h"
 #include "Status.cpp"
 #include "EventQueue.cpp"
 using namespace std;
@@ -65,9 +65,18 @@ int main(int argc, char** argv){
     s.insert(6);
     s.inorder();
 
-    s.remove(1);
-    s.inorder();
-    cout << *(s.searchR(1));
+    Point p1(1,1), p2(3,3);
+    Point p3(2,1), p4(4,3);
+    LineSegment l1(p1,p2);
+    LineSegment l2(p3,p4);
+    
+    Point p5;
+    p5 = l2.intersects_at(l1);
+    cout << p5.is_nan();
+    // cout << p5.x << " " << p5.y << endl;
+    // Point p6(1,-30);
+    // if(l1.contains_point(p6)) cout << "true" << endl;
+    // else cout << "false" << endl;
 
     return(0);
 }
--- src/primitives.h
+++ src/primitives.h
@@ -1,6 +1,9 @@
 #ifndef PRIMITIVES
 #define PRIMITIVES
 
+#include<iostream>
+#include<cmath>
+
 typedef long double coordinate;
 typedef long double len;
 
@@ -11,6 +14,7 @@ class Point{
         Point();
         Point(coordinate x_in, coordinate y_in);
         bool operator== (const Point& p2);
+        bool is_nan();
 };
 
 class LineSegment{
@@ -20,9 +24,8 @@ class LineSegment{
     public:
         LineSegment(Point p1, Point p2);
         bool contains_point(Point pt);
-        bool intersects_with(LineSegment ls);
+        Point intersects_at(LineSegment ls);
         bool operator== (const LineSegment& l2);
-
         len length();
         Point start_pt();
         Point end_pt();
