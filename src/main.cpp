af32cfb1ebe17181495041b3c296d3164309810f
Basu Dubey <basu.96@gmail.com>
2019-04-02 16:52:35 +0530

    Pass input file as command line argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 #include <vector>
 #include <thread>
 #include <mutex>
@@ -6,8 +8,37 @@
 #include "LSIGraphix.h"
 using namespace std;
 
+#define DEFAULT_FILENAME "../src/test.txt"
+
 void init_graphix_class(LSIGraphix* x){
     x->loopie();
+}
+
+
+void get_lines_from_file(string filename, vector<LineSegment>& all_lines){
+    all_lines.clear();
+    ifstream fin;
+    fin.open(filename, ios::in);
+    string p1,p2;
+    string comma = ",";
+    while(fin >> p1 >> p2){
+
+        p1 = p1.substr(1, p1.size() - 2);
+        p2 = p2.substr(1, p2.size() - 2);
+
+        string p1_x = p1.substr(0,p1.find(comma));
+        string p1_y = p1.substr(p1.find(comma) + 1, p1.size());
+
+        string p2_x = p2.substr(0,p2.find(comma));
+        string p2_y = p2.substr(p2.find(comma) + 1, p2.size());
+
+        Point point_1(stold(p1_x), stold(p1_y));
+        Point point_2(stold(p2_x), stold(p2_y));
+        LineSegment new_line(point_1, point_2);
+
+        all_lines.push_back(new_line);
+    }
+    fin.close();
 }
 
 int main(int argc, char** argv){
@@ -16,44 +47,20 @@ int main(int argc, char** argv){
     LSIGraphix gfx(mtx);
     thread t1(init_graphix_class, &gfx);
 
-    Point p1(-5,-5), p2(5,5);
-    LineSegment l1(p1,p2);
+    string filename;
+    if(argc > 1){
+        filename = argv[1];
+        cout << filename << endl;
+    } else {
+        filename = DEFAULT_FILENAME;
+    }
 
-    Point p3(-4,4), p4(6,-6);
-    LineSegment l2(p3,p4);
+    vector<LineSegment> all_lines;
+    get_lines_from_file(filename, all_lines);
 
-    Point p5(0,6), p6(0,1.5);
-    LineSegment l3(p5,p6);
-
-    Point p7(3,8), p8(3,-10);
-    LineSegment l4(p7,p8);
-
-    Point p9(0,1), p10(0,-5);
-    LineSegment l5(p9,p10);
-
-    //Point p11(1,9),p12(2,9),p13(4,10);
-    Point p11(0,9.5),p12(2,9),p13(4,10);
-    LineSegment l6(p7,p11), l7(p7,p12), l8(p7,p13);
-
-    Point p14(1,5), p15(4,8);
-    LineSegment l9(p14,p15);
-//
-    Point p16(1,6), p17(4,9);
-    LineSegment l10(p16,p17);
-
-    Point p18(1,-11),p19(2,-11);
-    LineSegment l11(p18,p8), l12(p19,p8);
-
-    Point p20(-3,3), p21(-4,2);
-    LineSegment l13(p20,p21);
-
-    vector<LineSegment> inp {l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13};
-//    vector<LineSegment> inp {l1,l2};
-    LineSegmentIntersector lsi(inp, gfx);
-
+    LineSegmentIntersector lsi(all_lines, gfx);
     LSIResult res = lsi.computeIntersections();
     cout << "RESULT:\n";
-    //cout << res.size();
 
     for (auto i : res){
         cout << i.first << " : ";
